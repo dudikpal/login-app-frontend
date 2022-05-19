fe4b5dcac9d940b51133edef199d2aa945176dee
Dudik Pál <dudikpal@gmail.com>
2022-05-19 23:49:51 +0200

    Sign in get a response from back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'login-app-frontend';
+export class AppComponent implements OnInit{
+  title = 'Tic Tac Toe';
+  public isAuthenticated = false;
+
+  ngOnInit(): void {
+    this.isAuthenticated = true;
+  }
+
+
+
+
+  public logout(): void {
+    // todo
+  }
 }
