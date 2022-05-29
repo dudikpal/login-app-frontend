f86afdb59c68ad2dc992cca1167b8ba46e1edc02
Dudik Pál <dudikpal@gmail.com>
2022-05-29 14:38:10 +0200

    Authentication and authorization working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {HomeComponent} from "./home/home.component";
 import {LoggedInGuardService} from "./login/logged-in-guard.service";
-import {GarageComponent} from "./garage/garage.component";
+import {GarageComponent} from "./home/garage/garage.component";
 import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'home', component: HomeComponent/*, canActivate: [LoggedInGuardService]*/},
-  {path: 'garage', component: GarageComponent/*, canActivate: [LoggedInGuardService]*/},
+  {path: 'home', component: HomeComponent, canActivate: [LoggedInGuardService]},
+  {path: 'garage', component: GarageComponent, canActivate: [LoggedInGuardService]},
   //{path: '**', component: LoginComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,40 @@
-import { FlexLayoutModule } from '@angular/flex-layout';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatOptionModule} from '@angular/material/core';
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {LoggedInGuardService} from "./login/logged-in-guard.service";
-import { GarageComponent } from './garage/garage.component';
-import { RegisterComponent } from './register/register.component';
+import {GarageComponent} from './home/garage/garage.component';
+import {RegisterComponent} from './register/register.component';
 import {MatDividerModule} from "@angular/material/divider";
 import {MatGridListModule} from "@angular/material/grid-list";
 
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    GarageComponent,
-    RegisterComponent
-  ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent,
+        GarageComponent,
+        RegisterComponent,
+    ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
@@ -54,8 +55,9 @@ import {MatGridListModule} from "@angular/material/grid-list";
         ReactiveFormsModule,
         MatGridListModule,
     ],
-  providers: [LoggedInGuardService,
-  FormBuilder],
-  bootstrap: [AppComponent]
+    providers: [LoggedInGuardService,
+        FormBuilder,],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
